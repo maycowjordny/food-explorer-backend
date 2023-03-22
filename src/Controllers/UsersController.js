326172d43932e5121cfa1855a93f77646e899353
maycow <maycowjordny2015@gmail.com>
2023-03-22 11:37:30 -0300

    fazendo metodo update e metodo create do userController

--- src/Controllers/UsersController.js
+++ src/Controllers/UsersController.js
@@ -1,15 +1,67 @@
 const AppError = require("../utils/AppError")
+const { hash, compare } = require("bcryptjs")
+const knex = require('../database/knex')
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body
 
-        if (!name) {
-            throw new AppError("Nome é obrigatório", 401)
+        const hashedPassword = await hash(password, 12)
+
+        const checkEmailExist = await knex('USERS').where({ email }).first()
+        if (checkEmailExist) {
+            throw new AppError("E-mail já está sendo utilizado.")
         }
 
-        response.status(201).json({ name, email, password })
+        await knex('USERS').insert({
+            name,
+            email,
+            password: hashedPassword,
+        })
+
+        return response.status(201).json({ message: 'Usuário criado com sucesso!' })
     }
+
+    async update(request, response) {
+        const { name, email, password, old_password } = request.body
+        const userId = request.user.id
+        try {
+
+            const checkUserExist = await knex('USERS').where({ id: userId })
+
+            if (!checkUserExist) {
+                throw new AppError("Usuário não encontrado")
+            }
+
+            const userWithUpdatedEmail = await knex('USERS').where({ email })
+
+            if (userWithUpdatedEmail && userWithUpdatedEmail.id !== checkUserExist.id) {
+                throw new AppError("Este e-mail já está em uso.")
+            }
+
+
+
+
+        } catch (error) {
+            console.log(error)
+
+            return response.status(500).json({ error: "Erro ao atualizar o usuário" })
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
 
 module.exports = UsersController
--- src/database/knex/migrations/20230321173352_createUser.js
+++ src/database/knex/migrations/20230321173352_createUser.js
@@ -2,7 +2,7 @@
 exports.up = knex => knex.schema.createTable('USERS', table => {
     table.increments('id').primary()
     table.text('name').notNullable()
-    table.text('email').unique().notNullable()
+    table.text('email').notNullable()
     table.text('password').notNullable()
     table.boolean('is_admin').defaultTo(false)
     table.text('avatar')
